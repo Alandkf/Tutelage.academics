19463b54cc355d816e41fdc4647e6c8fb996079a
r3k4r <rekarjamal33@gmail.com>
2025-09-07 10:28:57 +0300

    k

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: false,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.coe.int'
+      }
+    ],
+    dangerouslyAllowSVG: true, 
+  },
+}
 
 export default nextConfig;
