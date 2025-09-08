1f6f3d21ef00b54ea59dbecd02be0523cff5eed9
r3k4r <rekarjamal33@gmail.com>
2025-09-08 15:22:28 +0300

    database set

--- backend/models/index.js
+++ backend/models/index.js
@@ -15,21 +15,23 @@ const env = process.env.NODE_ENV || 'development';
 
 
 const sequelize = new Sequelize(
-process.env.DB_NAME || 'Tutelage.academics',
-process.env.DB_USER || 'root',
-process.env.DB_PASS || '(Aland&DB)',
-{
-host: process.env.DB_HOST || 'localhost',
-// port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-dialect: 'mysql',
-// logging: process.env.SEQ_LOG === 'true' ? console.log : false,
-// define: {
-// underscored: true,
-// freezeTableName: false,
-// timestamps: true
-// }
-}
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  }
 );
+
 
 
 const db = {};
