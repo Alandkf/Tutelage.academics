270b88b29b50d7bd2ed3e6767c06f10930f6082e
r3k4r <rekarjamal33@gmail.com>
2025-09-09 17:33:06 +0300

    k

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,12 +7,9 @@ const passport = require('passport');
 const AuthController = require('../controllers/authController');
 
 // Authentication routes
-router.get('/', AuthController.get); // this route is no need, since frontend could handle it itself
-router.get('/register', AuthController.getRegister); // this route is no need, since frontend could handle it itself
 router.post('/register', AuthController.register);
-router.get('/login', AuthController.getLogin); // this route is no need, since frontend could handle it itself
 router.post('/login', AuthController.login);
-router.post('/refresh-token', AuthController.refreshToken); // completed
+router.post('/refresh-token', AuthController.refreshToken); 
 router.post('/logout', AuthController.logout);
 router.get('/me', isAuthenticated, AuthController.getCurrentUser);
 
