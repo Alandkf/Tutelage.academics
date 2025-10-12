717246e6f3cef649b440c2bcf3ef083459a64e12
r3k4r <rekarjamal33@gmail.com>
2025-10-12 20:38:51 +0300

    k

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,6 +4,7 @@ import GlobalTestMastery from '@/components/landing/GlobalTestMastery '
 import HeroSection from '@/components/landing/Hero'
 import ImproveYourEnglishSkills from '@/components/landing/ImproveYourEnglishSkills'
 import OnlineCourses from '@/components/landing/OnlineCourses'
+import Practice from '@/components/landing/Practice'
 import Resources from '@/components/landing/Resources '
 import Tests from '@/components/landing/Tests'
 import TutelageAi from '@/components/landing/TutelageAi'
@@ -19,6 +20,7 @@ const Home = () => {
   <OnlineCourses />
   <Tests />
   <GlobalTestMastery />
+  <Practice />
   <ImproveYourEnglishSkills />
    </>
   )
---
+++ frontend/components/landing/Practice.jsx
@@ -0,0 +1,9 @@
+
+
+const Practice = () => {
+  return (
+    <div className=''>Practice anytime with our free resources</div>
+  )
+}
+
+export default Practice
