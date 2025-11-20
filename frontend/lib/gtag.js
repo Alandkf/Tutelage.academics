a13e09d07ff3df864da65662edee0af37f16fc8d
r3k4r <rekarjamal33@gmail.com>
2025-11-20 19:26:07 +0300

    Update gtag.js

--- frontend/lib/gtag.js
+++ frontend/lib/gtag.js
@@ -1,4 +1,3 @@
-// lib/gtag.js
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 
 export const pageview = (url) => {
@@ -6,7 +5,6 @@ export const pageview = (url) => {
 
   const isLocalhost = window.location.hostname === 'localhost'
 
-  // Config for GA
   window.gtag('config', GA_MEASUREMENT_ID, {
     page_path: url,
     page_title: document.title || url,
@@ -14,7 +12,6 @@ export const pageview = (url) => {
     ...(isLocalhost ? {} : { traffic_type: 'internal' })
   })
 
-  // Also send page_view event
   window.gtag('event', 'page_view', {
     page_path: url,
     page_title: document.title || url,
